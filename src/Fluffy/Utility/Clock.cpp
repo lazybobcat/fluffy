3c76da608109ef0cab6fee398f38066b0ea9b617
Loïc Boutter <loic.boutter@gmail.com>
2017-11-16 09:14:15 +0100

    Add Clock class

---
+++ src/Fluffy/Utility/Clock.cpp
@@ -0,0 +1,23 @@
+//
+// Fluffy
+// @author Lo-X
+// @website http://www.loicboutter.fr
+// @copyright 2016 All rights reserved
+// File created by lo-x on 14/11/17.
+//
+
+#include <Fluffy/Utility/Clock.hpp>
+
+using namespace Fluffy::Utility;
+
+Clock::Clock()
+{
+    mStartPoint = std::chrono::steady_clock::now();
+}
+
+Time Clock::elapsedTime() const
+{
+    auto now = std::chrono::steady_clock::now();
+
+    return microseconds(std::chrono::duration_cast<std::chrono::microseconds>(now - mStartPoint));
+}
