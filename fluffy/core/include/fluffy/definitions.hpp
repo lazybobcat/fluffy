0060c0992542f9f66c996b353cc2951a3a85ad51
Boutter Loïc <loic.boutter@gmail.com>
2021-01-25 12:09:25 +0100

    :truck: Moved fluffy source files

---
+++ fluffy/core/include/fluffy/definitions.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+// General configurations
+#define FLUFFY_VERSION "0.1.0"
+#define FLUFFY_LOG_FILE "logs.txt"
+#define FLUFFY_PROFILE_FILE "scope_profile.json"
+#define FLUFFY_DEBUG_COMPACT_JSON true
+
+#ifdef FLUFFY_DEBUG
+#define FLUFFY_PROFILING_ACTIVE
+#endif
+
+// ECS configurations
+#define FLUFFY_ECS_MAX_COMPONENTS 128;
+
+// OpenGL configurations
+#define OPENGL_MAJOR_VERSION 3
+#define OPENGL_MINOR_VERSION 3
+
+namespace Fluffy {
+template<typename T>
+using Ref = std::shared_ptr<T>;
+template<typename T, typename... Args>
+constexpr Ref<T> CreateRef(Args&&... args)
+{
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+template<typename T>
+using Unique = std::unique_ptr<T>;
+template<typename T, typename... Args>
+constexpr Unique<T> CreateUnique(Args&&... args)
+{
+    return std::make_unique<T>(std::forward<Args>(args)...);
+}
+
+using Char32   = std::uint32_t;
+using String   = std::string;
+using String32 = std::u32string;
+}
